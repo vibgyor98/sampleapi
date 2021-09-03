fecfd2ea449aaacd1b383dfc585ea3500288968a
souravkar <sourav.kar@nagarro.com>
2021-09-03 10:34:22 +0530

    initial commit

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var persons = new string[]{
+                "Barcelona",
+                "Real Madrid",
+                "Manchester City",
+                "Manchester United"
+            };
+            return Ok(persons);
+        }
+    }
+}
