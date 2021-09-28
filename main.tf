501549e1b0be9e6a0d9fe18f3fd460b0696406b4
souravkar <sourav.kar@nagarro.com>
2021-09-28 11:32:45 +0530

    updated app_service_plan kind = Linux

--- main.tf
+++ main.tf
@@ -66,6 +66,8 @@ resource "azurerm_app_service_plan" "app_plan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Standard"
